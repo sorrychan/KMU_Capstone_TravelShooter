8d1c06184fb77547ca06f163ee6b37057fbe58cf
sryChan <dogdegdog@gmail.com>
2019-10-06 22:52:05 +0900

    레벨 기초 구상+ 투사체
    마우스 클릭으로 첫 목표를 만들고 누른상태에서 움직일 수 있고 마우스를 떼면 발사. 투사체가 날아가는 동안 다시 새롭게 목표를 찍어도 기존의 것과 별개로 취급해 하나의 투사체를 발사해 날아가는 중에도 다음거 위치 조절 가능

---
+++ TravelShooter/Assets/2.Scripts/SpawnOnMousePos.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnMousePos : MonoBehaviour
+{
+    public float speed;
+    public bool ball = false;
+    public GameObject S;
+    public float fAniTime = 3.0f;
+    float fTimeCalc = 0.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "bullet")
+        {
+            return;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && ball == false)
+        {
+            fTimeCalc = Time.time + fAniTime;
+
+            Vector3 mos = Input.mousePosition;
+            mos.z = Camera.main.farClipPlane;
+
+            Vector3 dir = Camera.main.ScreenToWorldPoint(mos);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.transform.position, dir, out hit, mos.z))
+            {
+                Debug.Log(hit.point);
+                GetComponent<Rigidbody>().AddForce(new Vector3(hit.point.x - transform.position.x, 0, hit.point.z - transform.position.z) * speed);
+            }
+            ball = true;
+        }
+
+        if (Time.time > fTimeCalc && ball == true)
+        {
+            Destroy(gameObject);
+            Instantiate(S, new Vector3(1, 12, -14), transform.rotation);
+            //fTimeCalc = Time.time + fAniTime;
+            ball = false;
+            //Debug.Log("destroy");
+        }
+    }
+
+}
