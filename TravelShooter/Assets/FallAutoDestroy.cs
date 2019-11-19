303ba3e138c261bda38336b563e934dda11a4b82
KCS <dogdegdog@gmail.com>
2019-11-20 01:03:54 +0900

    나무 쓰러진거 자동 사라짐, 스테이지11까지 테스트 완료

---
+++ TravelShooter/Assets/FallAutoDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallAutoDestroy : MonoBehaviour
+{
+    public float DeadCount = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "PLANES")
+        {
+            Destroy(gameObject, DeadCount);
+        }
+    }
+}
