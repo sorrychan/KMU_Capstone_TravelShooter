fad64a69dc5fe6c5652e8d92b0d1948033b0558c
JDH <jdh5470417@gmail.com>
2019-12-07 20:52:09 +0900

    FadeInOut적용
    Object씬에 페이드 인 아웃 적용
    카메라의 FadeInOut.cs코드를 사용해 조절

---
+++ TravelShooter/Assets/2.Scripts/FadeInOut.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeInOut : MonoBehaviour
+{
+
+    public Easing.Type easing;
+
+    void Start()
+    {
+        Camera.main.FadeIn(5f, easing);         //페이드 인
+        //Camera.main.FadeOut(5f, easing);      //페이드 아웃
+    }
+}
