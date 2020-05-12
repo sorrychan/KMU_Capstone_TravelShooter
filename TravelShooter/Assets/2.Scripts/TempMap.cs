2a76795abdd34e9073c813d59c4d63376b87bdfe
JDH <jdh5470417@gmail.com>
2020-05-12 16:14:41 +0900

    데이터베이스 맵 로드 기능 구현
    데이터베이스의 데이터를 기반으로 맵을 로드하는 기능을 구현
    타일맵 에디터에서 스테이지 클리어, 실패가 인식되도록 수정

---
+++ TravelShooter/Assets/2.Scripts/TempMap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempMap : MonoBehaviour
+{
+    public int[] MapData = new int[35];
+
+    private void Start()
+    {
+        for (int i = 0; i < 35; i++)
+        {
+            MapData[i] = 0;
+        }
+    }
+}
---
+++ TravelShooter/Assets/2.Scripts/UserMapButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserMapButton : MonoBehaviour
+{
+    public GameObject Canvas;
+
+    private void Start()
+    {
+        Canvas = GameObject.Find("Canvas");
+    }
+
+    public void OnLoadMap()
+    {
+        gameObject.transform.parent.SendMessage("LoadMap");
+        Canvas.SetActive(false);
+    }
+}
